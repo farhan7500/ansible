bba5620c85ff1f6838c435639266af561eb88282
Farhan Nomani <nomani@hpe.com>
2018-05-11 06:04:52 -0400

    Review address: aplying review comments

---
+++ lib/ansible/module_utils/hpe3par.py
@@ -0,0 +1,78 @@
+def cpg_argument_spec():
+    return {
+        "state": {
+            "required": True,
+            "choices": ['present', 'absent'],
+            "type": 'str'
+        },
+        "storage_system_ip": {
+            "required": True,
+            "type": "str"
+        },
+        "storage_system_port": {
+            "required": True,
+            "type": "str"
+        },
+        "storage_system_username": {
+            "required": True,
+            "type": "str",
+            "no_log": True
+        },
+        "storage_system_password": {
+            "required": True,
+            "type": "str",
+            "no_log": True
+        },
+        "cpg_name": {
+            "required": True,
+            "type": "str"
+        },
+        "domain": {
+            "type": "str"
+        },
+        "growth_increment": {
+            "type": "float",
+            "default": -1.0
+        },
+        "growth_increment_unit": {
+            "type": "str",
+            "choices": ['TiB', 'GiB', 'MiB'],
+            "default": 'GiB'
+        },
+        "growth_limit": {
+            "type": "float",
+            "default": -1.0
+        },
+        "growth_limit_unit": {
+            "type": "str",
+            "choices": ['TiB', 'GiB', 'MiB'],
+            "default": 'GiB'
+        },
+        "growth_warning": {
+            "type": "float",
+            "default": -1.0
+        },
+        "growth_warning_unit": {
+            "type": "str",
+            "choices": ['TiB', 'GiB', 'MiB'],
+            "default": 'GiB'
+        },
+        "raid_type": {
+            "required": False,
+            "type": "str",
+            "choices": ['R0', 'R1', 'R5', 'R6'],
+        },
+        "set_size": {
+            "required": False,
+            "type": "int",
+            "default": -1
+        },
+        "high_availability": {
+            "type": "str",
+            "choices": ['PORT', 'CAGE', 'MAG'],
+        },
+        "disk_type": {
+            "type": "str",
+            "choices": ['FC', 'NL', 'SSD'],
+        }
+    }
