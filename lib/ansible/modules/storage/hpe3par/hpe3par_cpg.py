3fb312b67bc6292cb9143c859b3299d9913694c4
sathishkumarsivagurunathan <sathish1000@icloud.com>
2018-06-21 09:58:35 +0530

    Adding GPL3.0+ License (#7)
    * Update hpe3par.py
    
    * Update hpe3par_cpg.py
    
    * Update hpe3par_cpg.py

--- lib/ansible/modules/storage/hpe3par/hpe3par_cpg.py
+++ lib/ansible/modules/storage/hpe3par/hpe3par_cpg.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python
 
-# (C) Copyright 2018 Hewlett Packard Enterprise Development LP
+# (C) Copyright [2018] Hewlett Packard Enterprise Development LP
 #
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of version 3 of the GNU General Public License as
-# published by the Free Software Foundation.  Alternatively, at your
+# This program is free software; you can redistribute it and/or modify it
+# under the terms of version 3 or later of the GNU General Public License
+# as published by the Free Software Foundation.  Alternatively, at your
 # choice, you may also redistribute it and/or modify it under the terms
 # of the Apache License, version 2.0, available at
 #
-#     http://www.apache.org/licenses/LICENSE-2.0
+#    http://www.apache.org/licenses/LICENSE-2.0
 #
 # This program is distributed in the hope that it will be useful, but
 # WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
@@ -16,7 +16,7 @@
 # for more details.
 #
 # You should have received a copy of the GNU General Public License along
-# with this program. If not, see <https://www.gnu.org/licenses/>
+# with this program.  If not, see <https://www.gnu.org/licenses/>
 
 from __future__ import absolute_import, division, print_function
 __metaclass__ = type
